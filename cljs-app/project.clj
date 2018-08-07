c0028e7036a8cee9660424eacb8db4d136eca9aa
zhuzha <denis.yagovkin@ofd-ya.ru>
2018-08-07 15:15:36 +0300

    hiccups

--- cljs-app/project.clj
+++ cljs-app/project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.339"]]
+                 [org.clojure/clojurescript "1.10.339"]
+                 [hiccups "0.3.0"]]
+
   :plugins [[lein-cljsbuild "1.1.7"]]
   :cljsbuild
   {:builds [{:source-paths ["src"]
